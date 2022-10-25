330e32d2f1e3e3274f20ab37e9684d13fda1cdbe
Chima-Expert <chima@asktoexpert.com>
2022-10-25 13:13:37 +0100

    Fixing build errors 3

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,15 +4,21 @@ export default function MyDoc() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" async />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+          async
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;500;600;700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100&display=swap"
           rel="stylesheet"
+          async
         />
         <script
           src="https://code.iconify.design/iconify-icon/1.0.1/iconify-icon.min.js"
-          defer
+          async
         ></script>
       </Head>
       <div id="modal-root"></div>
