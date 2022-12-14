870247b563e07671560c97cba0f27152a907c7e2
Chima-Expert <chima@asktoexpert.com>
2022-12-14 12:29:16 +0100

    Attempted to resolve CORS error - 1

--- next.config.js
+++ next.config.js
@@ -7,7 +7,44 @@ const nextConfig = {
     additionalData: `@import "src/styles/sass/abstracts/variables.scss";`,
   },
   images: {
-    domains: ['plus.unsplash.com', 'images.unsplash.com', 'lh3.googleusercontent.com', 'platform-lookaside.fbsbx.com'],
+    domains: [
+      'plus.unsplash.com',
+      'images.unsplash.com',
+      'lh3.googleusercontent.com',
+      'platform-lookaside.fbsbx.com',
+    ],
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://localinspire.vercel.app',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:path',
+        destination: 'https://local-inspire-backend.onrender.com/api/v1/:path',
+      },
+    ];
   },
 };
 
