e46bc2d651992cedc06ee2198574ac79902b9efe
Chima-Expert <chima@asktoexpert.com>
2022-12-20 03:51:00 +0100

    Improved Home gallery photos in mobile view. Made popular categories in Home header to be linked to correspond to business search results page

--- src/features/components/home/Gallery.tsx
+++ src/features/components/home/Gallery.tsx
@@ -17,6 +17,7 @@ function Gallery() {
             style={{ borderRadius: '8px' }}
           />
         </picture>
+
         <picture>
           <Image
             src="/img/pexels-pixabay-262978.jpg"
@@ -27,6 +28,7 @@ function Gallery() {
             style={{ borderRadius: '8px' }}
           />
         </picture>
+
         <picture>
           <Image
             src="/img/pexels-suzukii-xingfu-872831.jpg"
@@ -38,19 +40,23 @@ function Gallery() {
           />
         </picture>
       </div>
+
       <div className={styles['gallery-text']}>
         <h2 className="mb-4" style={{ maxWidth: '17ch' }}>
           <span className="text-pry">Join us</span> in making our community great!
         </h2>
-        <p className="mb-3">
+
+        <p className="mb-3 fs-5">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam alias expedita
           ratione voluptates! Veritatis quidem odio iste nesciunt, ratione nemo.
         </p>
-        <p className="mb-3">
+
+        <p className="mb-3 fs-5">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam assumenda aut
           nostrum dicta. Quis fugiat provident, dolores quam hic obcaecati.
         </p>
-        <p>
+
+        <p className=" fs-5">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam assumenda aut
           nostrum dicta. Quis fugiat provident, dolores quam hic obcaecati.
         </p>
--- src/features/components/home/header/HeaderServices.tsx
+++ src/features/components/home/header/HeaderServices.tsx
@@ -1,8 +1,15 @@
 import cls from 'classnames';
 import Image from 'next/image';
+import Link from 'next/link';
+import useCurrentLocation from '../../../hooks/useCurrentLocation';
 import styles from './Header.module.scss';
 
+import * as urlUtils from '../../../utils/url-utils';
+
 function HeaderServices() {
+  const { state } = useCurrentLocation();
+  const [stateName, stateCode] = (state || '').split(', ');
+
   const services = [
     { id: 'hotels', name: 'Hotels', icon: '/icons/restaurant-icon.png' },
     { id: 'restaurants', name: 'Restaurants', icon: '/icons/hotel-icon.png' },
@@ -19,10 +26,23 @@ function HeaderServices() {
     <ul className={cls(styles['header-services'], 'no-bullets')}>
       {services.map(({ id, name, icon }) => (
         <li key={id}>
-          <a href="">
-            <Image src={icon} alt={name} width={30} height="30" />
-            <span>{name}</span>
-          </a>
+          <Link
+            href={
+              state
+                ? urlUtils.getBusinessSearchResultsUrl({
+                    category: id,
+                    city: stateName,
+                    stateCode,
+                  })
+                : '/'
+            }
+            passHref
+          >
+            <a>
+              <Image src={icon} alt={name} width={30} height="30" />
+              <span>{name}</span>
+            </a>
+          </Link>
         </li>
       ))}
     </ul>
