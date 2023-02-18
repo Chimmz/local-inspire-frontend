7579e14bec264f5518b078e43f7a5bf68d6e55fb
Chima-Expert <chima@asktoexpert.com>
2023-02-18 09:12:55 +0100

    Attempted fixing build errors - 5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,24 +2,19 @@ import { useMemo } from 'react';
 import { GetServerSideProps, GetStaticProps, NextPage } from 'next';
 import { getProviders, signOut } from 'next-auth/react';
 
-import AuthContextProvider, { useAuthContext } from '../features/contexts/AuthContext';
+import AuthContextProvider from '../features/contexts/AuthContext';
 
 import Header from '../features/components/home/header/Header';
 import Gallery from '../features/components/home/Gallery';
 import BestPlaces from '../features/components/home/BestPlaces';
 import Layout from '../features/components/layout/index';
 import Navbar from '../features/components/layout/navbar/Navbar';
-import { useRouter } from 'next/router';
 
 interface HomePageProps {
   popularCategorySuggestions: string[];
 }
 
 function HomePage({ popularCategorySuggestions }: HomePageProps) {
-  // const router = useRouter();
-  // if (!!router.query.authError) signOut({ redirect: false });
-  // const {showAuthModal} = useAuthContext()
-
   return (
     <AuthContextProvider>
       <Layout>
